ce73d6073fdc1c1a02358d6bad663be3e8985150
Bernt Christian Egeland <cbegelan@gmail.com>
2022-11-24 11:57:14 +0100

    timeago

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,37 +1,39 @@
-"use client"
+'use client';
 
-import { useCallback, useEffect, useState } from 'react';
-import styles from './page.module.css'
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import { useQuery } from '@tanstack/react-query';
-import ReactTimeAgo from 'react-time-ago'
+import TimeAgo from 'react-timeago';
 
-const socket = io("https://iotsrv1.egeland.io");
+const socket = io('https://iotsrv1.egeland.io');
 
-const iot:any = [{}]
+const iot: any = [{}];
 interface IDevice {
-  datetime: Date
-  friendly_name: string
-  heater: string
-  "iot-device": string
-  electric_time_to_start: string
-  fuel_time_to_start: string
-  uptime: string
+  datetime: Date;
+  friendly_name: string;
+  heater: string;
+  'iot-device': string;
+  electric_time_to_start: string;
+  fuel_time_to_start: string;
+  uptime: string;
 }
 
-const iots:any = []
+const iots: any = [];
 
-const fetchData = async () =>{
-  const response = await fetch("https://iotsrv1.egeland.io/devices")
-  return response.json()
-}
+const fetchData = async () => {
+  const response = await fetch('https://iotsrv1.egeland.io/devices');
+  return response.json();
+};
 
-export default function Home() {
+const App = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [lastPong, setLastPong] = useState("");
+  const [lastPong, setLastPong] = useState('');
   const [iotDevices, setIotDevices] = useState<any>([]);
 
-  const { isLoading, isError, data, error } = useQuery({ queryKey: ['devices'], queryFn: fetchData })
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['devices'],
+    queryFn: fetchData,
+  });
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -43,20 +45,22 @@ export default function Home() {
     });
 
     socket.on('iot-ping', (devices) => {
-      const msg: IDevice = JSON.parse(devices)
+      const msg: IDevice = JSON.parse(devices);
       console.log(iotDevices);
-      const dev =  [...iotDevices]
+      const dev = [...iotDevices];
 
-      // check if device exsist in list 
-      const index = iotDevices.findIndex((f:any) => f["iot-device"] === msg["iot-device"])
+      // check if device exsist in list
+      const index = iotDevices.findIndex(
+        (f: any) => f['iot-device'] === msg['iot-device']
+      );
 
       // update or add
-      if(index !== -1) {
-        dev[index] = msg
-        return setIotDevices(dev)
+      if (index !== -1) {
+        dev[index] = msg;
+        return setIotDevices(dev);
       }
 
-      setIotDevices((prev:any) => [...prev, {...msg}])
+      setIotDevices((prev: any) => [...prev, { ...msg }]);
     });
 
     return () => {
@@ -66,57 +70,69 @@ export default function Home() {
     };
   }, [iotDevices]);
 
-  useEffect(()=>{
-    if(!data?.length) return
-    setIotDevices(data)
-  },[data])
-  
+  useEffect(() => {
+    if (!data?.length) return;
+    setIotDevices(data);
+  }, [data]);
 
-  if(isLoading) return <div className="flex justify-center text-2xl ">Loading IoT devices...</div>
+  if (isLoading)
+    return (
+      <div className='flex justify-center text-2xl '>
+        Loading IoT devices...
+      </div>
+    );
 
   return (
-      <main>
-        <div className="flex justify-center">
-          {iotDevices.map((d:any,idx:any)=>{
-            return <div className='mt-5' key={d["iot-device"]}>
-                <div className='flex flex-col justify-between border rounded-lg m-4 p-4 w-80'>
-                  <div className='flex justify-center mb-2'>{d.friendly_name}</div>
-                  <div className='flex justify-between'>
-                    <span>System:</span>
-                    <span>{d.system}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Lastseen:</span>
-                    <span><ReactTimeAgo date={new Date(d.datetime)} locale="en-US" /></span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Uptime:</span>
-                    <span>{d.uptime}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Heater:</span>
-                    <span>{d.heater}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Electric scheduled:</span>
-                    <span>{d.electric_time_to_start}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Fuel scheduled:</span>
-                    <span>{d.fuel_time_to_start}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Electric kWh price:</span>
-                    <span>{d.electric_price}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Fuel kWh price:</span>
-                    <span>{d.fuel_price}</span>
-                  </div>
+    <main>
+      <div className='flex justify-center'>
+        {iotDevices.map((d: any, idx: any) => {
+          return (
+            <div className='mt-5' key={d['iot-device']}>
+              <div className='flex flex-col justify-between border rounded-lg m-4 p-4 w-80'>
+                <div className='flex justify-center mb-2'>
+                  {d.friendly_name}
+                </div>
+                <div className='flex justify-between'>
+                  <span>System:</span>
+                  <span>{d.system}</span>
+                </div>
+                <div className='flex justify-between'>
+                  <span>Lastseen:</span>
+                  <span>
+                    <TimeAgo date={new Date(d.datetime)} />
+                  </span>
+                </div>
+                <div className='flex justify-between'>
+                  <span>Uptime:</span>
+                  <span>{d.uptime}</span>
+                </div>
+                <div className='flex justify-between'>
+                  <span>Heater:</span>
+                  <span>{d.heater}</span>
+                </div>
+                <div className='flex justify-between'>
+                  <span>Electric scheduled:</span>
+                  <span>{d.electric_time_to_start}</span>
+                </div>
+                <div className='flex justify-between'>
+                  <span>Fuel scheduled:</span>
+                  <span>{d.fuel_time_to_start}</span>
+                </div>
+                <div className='flex justify-between'>
+                  <span>Electric kWh price:</span>
+                  <span>{d.electric_price}</span>
+                </div>
+                <div className='flex justify-between'>
+                  <span>Fuel kWh price:</span>
+                  <span>{d.fuel_price}</span>
                 </div>
               </div>
-          })}
-        </div>
-      </main>
-  )
-}
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default App;
