ec4f687f9788420a1a69ff2ab7879869c5ce3e4b
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2022-11-25 14:41:27 +0100

    isntall

--- iot/provisioning.sh
+++ iot/provisioning.sh
@@ -4,15 +4,22 @@ set -m
 
 sudo apt-get update -y
 sudo apt install git -y
-curl -fsSL https://test.docker.com -o test-docker.sh
-sudo sh test-docker.sh
-sudo systemctl enable docker
 
-sudo usermod -aG docker $USER
-newgrp docker
+if [[ $(which docker) && $(docker --version) ]]; then
+    echo "Docker is already installed"
 
-sudo apt-get install docker-compose
-sudo apt-get start docker
+  else
+    echo "Install docker"
+    curl -fsSL https://test.docker.com -o test-docker.sh
+    sudo sh test-docker.sh
+    sudo systemctl enable docker
+
+    sudo usermod -aG docker $USER
+    newgrp docker
+
+    sudo apt-get install docker-compose
+    sudo apt-get start docker
+fi
 
 sudo git clone \
   --depth 1  \
@@ -25,7 +32,10 @@ cd ae-iot
 sudo git sparse-checkout set iot
 
 sudo chmod +x provisioning.sh
+printf "\n\n"
+read -p 'type client id (without spaces) NOTE! has to start iot: ' client_id
 
-read -p 'type client id (without spaces): ' client_id
+sed -i "/^\(client_id: \).*/s//\1'$client_id'/" $(pwd)/iot/config.yaml
 
-sed -i "/^\(client_id: \).*/s//\1'$client_id'/" config.yml
+cd iot
+docker-compose up -d
