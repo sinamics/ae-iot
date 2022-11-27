466f4388c7b037fe8dacdba958fe65355930356e
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2022-11-27 21:35:47 +0100

    dark theme default

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,15 +30,15 @@ export default function RootLayout({
           <MantineProvider>{children}</MantineProvider>
         </QueryClientProvider>
 
-        <footer className='hidden absolute bottom-0 w-full p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-[rgb(54,58,63)]'>
-          <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+        <footer className='hidden absolute bottom-0 w-full p-4 shadow md:flex md:items-center md:justify-between md:p-6 bg-[rgb(54,58,63)]'>
+          <span className='text-sm sm:text-center text-gray-400'>
             © 2022{' '}
             <a href='https://linkme.egeland.io' className='hover:underline'>
               Kodea Solutions™
             </a>
             . All Rights Reserved.
           </span>
-          <ul className='flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0'>
+          <ul className='flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0'>
             <li>
               <a href='https://linkme.egeland.io/' className='hover:underline'>
                 Contact
