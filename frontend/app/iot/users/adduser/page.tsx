ab890ee82c142f1dfd63f6fe00010f83c85bb20c
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2022-12-11 18:58:55 +0100

    users

---
+++ frontend/app/iot/users/adduser/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddUser = () => {
+  return <div>Adduser</div>;
+};
+
+export default AddUser;
--- frontend/app/iot/users/page.tsx
+++ frontend/app/iot/users/page.tsx
@@ -1,5 +1,5 @@
 import { db } from '@/lib/db';
-import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
 import React, { use } from 'react';
 import UsersTable from './(components)/userTable';
 
@@ -11,9 +11,17 @@ const Users = async () => {
   const users = JSON.stringify(await fetchUsers());
 
   return (
-    <div>
+    <div className='container'>
       <div className='flex justify-center text-2xl pt-5'>Users</div>
-      <div className='container'>
+      <div>
+        <Link
+          href='/iot/users/adduser'
+          className='mt-3 uppercase rounded-l inline-block px-6 py-1.5 border border-gray-400 bg-transparent text-center text-sm  font-medium text-slate-300 hover:border-slate-200 hover:bg-slate-600 focus:z-10 focus:outline-none transition duration-150 ease-in-out'
+        >
+          Add User
+        </Link>
+      </div>
+      <div>
         <UsersTable users={users} />
       </div>
     </div>
