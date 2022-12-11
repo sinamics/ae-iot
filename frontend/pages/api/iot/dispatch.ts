7e767095f7147ed2e48001454b9c479036af4505
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2022-12-11 10:30:30 +0100

    dispatch

--- frontend/pages/api/iot/dispatch.ts
+++ frontend/pages/api/iot/dispatch.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: any) {
   try {
     const response = mqtt.publish(
       `iot/subscribe/${client_id}`,
-      JSON.stringify({ type, ...action })
+      JSON.stringify({ type, action })
     );
     res.status(200).json(response);
   } catch (error) {
